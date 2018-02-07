555e9e27f9bb515bc74d05b5cf3a582cd8f25af0
Surya Srikar Peruri <suryasrikar1997@gmail.com>
2018-02-07 19:34:21 +0530

    Redis code added

--- redis.js
+++ redis.js
@@ -1,2 +1,35 @@
 console.log("surya");
 
+var redis = require('redis'), set_size = 10;
+var client = redis.createClient(6379, 'localhost', {no_ready_check: true});
+//client.auth('password', function (err) {
+//    if (err) throw err;
+//});
+
+client.on('connect', function() {
+    console.log('Connected to Redis');
+});
+client.get("rest_discount", function (err, reply) {
+    if (err) throw err;
+    console.log("rest_discount = " + reply.toString());
+});
+/*
+client.sadd("bigset", "a member");
+client.sadd("bigset", "another member");
+
+while (set_size > 0) {
+    client.sadd("bigset", "member " + set_size);
+    set_size -= 1;
+}
+*/
+// multi chain with an individual callback
+client.multi()
+    .scard("bigset")
+    .smembers("bigset")
+    .dbsize()
+    .exec(function (err, replies) {
+        console.log("MULTI got " + replies.length + " replies");
+        replies.forEach(function (reply, index) {
+            console.log("Reply " + index + ": " + reply.toString());});
+        client.quit();
+    });
